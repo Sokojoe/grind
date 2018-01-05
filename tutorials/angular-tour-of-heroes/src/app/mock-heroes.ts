4cbeae0b9d672fd678f3469405e0ae96089e513a
Sokjoe <Sokojoey@gmail.com>
2018-01-04 21:02:33 -0500

    Did first 1/2 of Angular tourofheroes tutorial

---
+++ tutorials/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. Niceguy' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Batman'},
+  { id: 4, name: 'Ironman' },
+  { id: 5, name: 'Ironman 2.0' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' },
+  { id: 10, name: 'Sharknado' }
+];
